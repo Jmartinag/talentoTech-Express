2e9797aa36c31995797fa41a73736cb020321247
Jhon Martin <martinjhon@gmail.com>
2024-02-26 20:08:22 -0500

    Graphql

---
+++ graphql/schema.js
@@ -0,0 +1,36 @@
+const { buildSchema } = require('graphql')
+
+const schema = buildSchema(`
+    type: User {
+        id: ID!
+        name: String!
+        lastname: String!
+        email: String!
+        avatar: String!
+    }
+    type: Message {
+        id: ID!
+        body: String!
+        from: User!
+        to: User!
+        readed: Boolean
+    }
+    type: House {
+        id: ID!
+        address: String!
+        city: String!
+        state: String!
+        size: Int!
+        type: String!
+        zip_code: String!
+        code: String!
+        rooms: Int!
+        bathrooms: Int!
+        price: Int!
+        image: String!
+    }
+    type: Query {
+        hello: String
+    }`)
+
+module.exports = schema
