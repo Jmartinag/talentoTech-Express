6aceaa8a0d8b1138a92005885dbec88356eba113
Jhon Martin <martinjhon@gmail.com>
2024-03-26 19:53:53 -0500

    Borra password

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -30,6 +30,7 @@ class UserController {
                                  fullname: `${user.name} ${user.lastname}`
                 }, this.jwtSecret, { expiresIn: '1h' })
 
+        user.password = null       
         return {"status": "success", "token": token, "user": user}
 
         } catch (error) {
